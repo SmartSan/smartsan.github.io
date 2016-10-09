fc27353edc82903f68842e808a0ad482ec399029
SmartSan <SmartSan@users.noreply.github.com>
2016-10-09 08:07:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/articeone',function(req,res){
+app.get('/article-one',function(req,res){
     res.send('Artice 1 of sanjay is requested and will be served');
 });
 
-app.get('/articetwo',function(req,res){
+app.get('/article-two',function(req,res){
     res.send('Artice 2 of sanjay is requested and will be served');
 });
 
-app.get('/articethree',function(req,res){
+app.get('/article-three',function(req,res){
     res.send('Artice 3 of sanjay is requested and will be served');
 });
 app.get('/ui/style.css', function (req, res) {
