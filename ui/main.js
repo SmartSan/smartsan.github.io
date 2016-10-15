e8c6d16906cbece51cb16f82ce4a807449408a41
SmartSan <SmartSan@users.noreply.github.com>
2016-10-15 20:18:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,18 @@
 console.log('Loaded!');
+
+
+var element = document.getElementId('main-text');
+element.innerHTML='New value';
+
+var img=document.getElementById('dream');
+
+function moveRight()
+{
+    marginleft=marginleft+10;
+    img.style.marginLeft = marginLeft='px';
+    
+}
+img.onclick=function()
+{
+    var integral = setInterval(moveRight, 100);
+}
