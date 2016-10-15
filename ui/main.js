43050c8c518f81c06d7fae225e15dc43c5e803cc
SmartSan <SmartSan@users.noreply.github.com>
2016-10-15 20:24:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,14 +2,14 @@ console.log('Loaded!');
 
 
 var element = document.getElementById('main-text');
-element.innerHTML='New value';
+element.innerHTML = 'New value';
 
 //Move the image
 var img=document.getElementById('dream');
 
 function moveRight()
 {
-    marginLeft=marginLeft+10;
+    marginLeft = marginLeft+10;
     img.style.marginLeft = marginLeft='px';
     
 }
