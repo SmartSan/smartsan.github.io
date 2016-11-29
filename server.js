60b7bfd66a92ff17d4dcd756773404922a04f749
SmartSan <SmartSan@users.noreply.github.com>
2016-11-29 14:49:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -70,6 +70,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+
+app.get('/ui/
 app.get('/ui/typed.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'typed.js'));
 });
